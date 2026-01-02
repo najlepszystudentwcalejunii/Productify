b61728a413cdda5aa57b24ea75176152e3a5f377
Michal <michalek99_9@tlen.pl>
2026-01-02 17:04:43 +0100

    first commit

---
+++ backend/src/db/index.ts
@@ -0,0 +1,20 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { ENV } from "../config/env";
+import { Pool } from "pg";
+import * as schema from "./schema";
+
+if (ENV.DB_URL) {
+  throw new Error("DB_URL url is not set in env variable");
+}
+
+const pool = new Pool({ connectionString: ENV.DB_URL });
+
+pool.on("connect", () => {
+  console.log("Database connected successfully");
+});
+
+pool.on("error", (err) => {
+  console.error("Error when connecting to database", err);
+});
+
+export const db = drizzle({ client: pool, schema });
---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+
+const app = express();
+
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ENV.FRONTEND_URL,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    message:
+      "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
+app.listen(ENV.PORT, () =>
+  console.log("Server is up and running on port: " + ENV.PORT)
+);
