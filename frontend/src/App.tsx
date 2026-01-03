e38213200140e87a9faca62ea70f5bd9346af129
Michal <michalek99_9@tlen.pl>
2026-01-03 13:53:36 +0100

    Added themes to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Route, Routes } from "react-router";
+import NavBar from "./components/nav-bar";
+import HomePage from "./pages/home-page";
+import ProductPage from "./pages/product-page";
+import ProfilePage from "./pages/profile-page";
+import CreatePage from "./pages/create-page";
+import EditProductPage from "./pages/edit-product-page";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen ">
+      <NavBar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/product/:id" element={<ProductPage />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route>
+          <Route path="/create" element={<CreatePage />}></Route>
+          <Route path="/edit" element={<EditProductPage />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ frontend/src/pages/product-page.tsx
@@ -0,0 +1,5 @@
+const ProductPage = () => {
+  return <div>Home`</div>;
+};
+
+export default ProductPage;
