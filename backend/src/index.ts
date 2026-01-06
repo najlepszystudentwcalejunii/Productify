e6892e0389eb54845c2144793a4693780ec5870f
Michal <michalek99_9@tlen.pl>
2026-01-06 18:18:19 +0100

    deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import commentRoutes from "./routes/commentRoutes";
 import productRoutes from "./routes/productRoutes";
+import path from "path";
 
 const app = express();
 
@@ -22,7 +23,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
 
-app.get("/", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.json({
     message:
       "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
@@ -33,6 +34,16 @@ app.get("/", (req, res) => {
     },
   });
 });
+
+if (ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () =>
   console.log("Server is up and running on port: " + ENV.PORT)
